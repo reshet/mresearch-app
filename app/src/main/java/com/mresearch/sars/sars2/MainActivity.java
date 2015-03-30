c338ce2f28d97f897a4b5fe82efeb2ca37529ea9
reshet <reshet.ukr@gmail.com>
2015-03-31 00:14:09 +0300

    Change app icon. Add logging and enabled app-cache support.

--- app/src/main/java/com/mresearch/sars/sars2/MainActivity.java
+++ app/src/main/java/com/mresearch/sars/sars2/MainActivity.java
@@ -3,6 +3,7 @@ package com.mresearch.sars.sars2;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
+import android.net.ConnectivityManager;
 import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -17,26 +18,39 @@ import android.webkit.WebStorage;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import java.io.File;
+
 
 public class MainActivity extends Activity {
 
-    //private static final String SARS2_URL = "http://172.29.92.8:8080/";
+    //private static final String SARS2_URL = "http://192.168.0.104:8095/";
     private static final String SARS2_URL = "http://dev.survey-archive.com:8095/";
     private WebView mWebView;
+    final Activity activity = this;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mWebView = (WebView) findViewById(R.id.webview);
-        mWebView.setWebViewClient(new HelloWebViewClient());
+        //mWebView.setWebViewClient(new HelloWebViewClient());
         mWebView.setWebChromeClient(new WebChromeClient() {
             public boolean onConsoleMessage(ConsoleMessage cm) {
                 Log.d("SARS2-web-view: ", cm.message() + " -- From line "
                         + cm.lineNumber() + " of "
                         + cm.sourceId());
                 return true;
-            } });
+            }
+            public void onProgressChanged(WebView view, int progress) {
+                activity.setTitle("Загрузка...");
+                activity.setProgress(progress * 100);
+
+                if(progress == 100) {
+                    activity.setTitle(R.string.app_name);
+                }
+            }
+
+        });
 
         // включаем поддержку JavaScript
         mWebView.getSettings().setJavaScriptEnabled(true);
@@ -45,8 +59,28 @@ public class MainActivity extends Activity {
         settings.setJavaScriptEnabled(true);
         settings.setDatabaseEnabled(true);
         settings.setDomStorageEnabled(true);
+        settings.setAppCachePath(getApplication().getCacheDir().toString());
+        settings.setAppCacheEnabled(true);
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
-            mWebView.getSettings().setDatabasePath("/data/data/" + mWebView.getContext().getPackageName() + "/databases/");
+            settings.setDatabasePath("/data/data/" + mWebView.getContext().getPackageName() + "/databases/");
+            String appCachePath = getApplicationContext().getCacheDir()
+                    .getAbsolutePath();
+            boolean existsPath = new File(appCachePath).exists();
+            File dir = getCacheDir();
+
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+            settings.setAppCachePath(appCachePath);
+            settings.setAppCacheMaxSize(1024 * 1024 * 8);
+            settings.setAllowFileAccess(true);
+        }
+        ConnectivityManager cm = (ConnectivityManager) this.getSystemService(Activity.CONNECTIVITY_SERVICE);
+        if(cm != null && cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected()){
+            settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        }
+        else{
+            settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
         }
 
         if (savedInstanceState == null)
@@ -73,25 +107,6 @@ public class MainActivity extends Activity {
         // Restore the state of the WebView
         mWebView.restoreState(savedInstanceState);
     }
-
-    private class HelloWebViewClient extends WebViewClient
-    {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url)
-        {
-            view.loadUrl(url);
-            return true;
-        }
-    }
-
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
-//            mWebView.goBack();
-//            return true;
-//        }
-//        return super.onKeyDown(keyCode, event);
-//    }
 
     @Override
     public void onBackPressed() {
